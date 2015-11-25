c5638b3086266104c64d28023155cdc10aabebc3
dorduchovni <dor@falcore.co>
2015-11-25 10:40:28 +0200

    test

---
+++ src/sample/SheetToTranslate.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.util.List;
+
+/**
+ * Created by Dor on 11/19/15.
+ */
+public class SheetToTranslate {
+    public String sourceFileName;
+    public String platform;
+    public List<String> languages;
+
+}
